f3f28a0be7f0f85e6a74ac0119453440be6ae9fd
Khoirul <kumam1543@mhs.mdp.ac.id>
2023-12-16 14:45:04 +0700

    perbaikkan

--- lib/data/catering_data.dart
+++ lib/data/catering_data.dart
@@ -8,9 +8,6 @@ var catringList = [
     'Grilled salmon with red rice, Grilled salmon with red rice merupakan menu diet sehat yang berbahan ikan salmon dan juga nasi merah yang menjadi kombinasi yang sesuai untuk kebutuhan orang yang sedang menjalani hidup sehat atau diet',
     type: 'makanan diet',
     imageAsset: 'images/ayam_asam_manis ',
-    imageUrls: [
-      'https://ibb.co/jhj7zDh',
-    ],
     isFavorite: false,
 
   ),
@@ -21,12 +18,6 @@ var catringList = [
     'fried rice tongkol, merupakan menu diet yang berbahan dari nasi merah yang di goreng dengan menggunakan minyak kelapa dengan protein dari ikan tongkolnya, menjadikan menu ini menu yang enak namun sehat',
     type: 'makanan diet',
     imageAsset: 'images/fried ',
-    imageUrls: [
-      'https://ibb.co/jhj7zDh',
-      'https://media-cdn.tripadvisor.com/media/photo-o/08/30/84/fe/detail.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/07/9c/66/49/parc-du-monastere.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/07/64/89/fe/candi-mendut-temple.jpg',
-    ],
     isFavorite: false,
   ),
   Catering(
@@ -36,12 +27,6 @@ var catringList = [
     'ayam asam manis, menu ini merupakan makanan yang banyak diminati karena berbahan dasar ayam dengan dibaluri saus asam manis menjadikan menu ini sangat lezat dan juga enak namu tetap sehat',
     type: 'Makanan diet',
     imageAsset: 'images/prambanan.jpeg',
-    imageUrls: [
-      'http://media-cdn.tripadvisor.com/media/photo-o/11/d3/f4/02/main-prambanan-temple.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/11/d3/f3/fb/main-prambanan-temple.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/11/cd/21/b5/prambanan-diwaktu-malam.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/11/cd/21/a1/prambanan-yang-luas-dan.jpg',
-    ],
     isFavorite: false,
   ),
   Catering(
@@ -51,12 +36,6 @@ var catringList = [
     'dada ayam dengan salad, menu ini berbahan dasar dada ayam yang mengandung protein yang sangat baik untuk memenuhi kebutuhan dalam diet',
     type: 'Makanan diet',
     imageAsset: 'images/lumbung.jpeg',
-    imageUrls: [
-      'https://media-cdn.tripadvisor.com/media/photo-o/28/11/e1/80/lumbung-temple.jpg',
-      'http://media-cdn.tripadvisor.com/media/photo-o/28/11/e1/73/lumbung-temple.jpg',
-      'http://media-cdn.tripadvisor.com/media/photo-o/28/11/e1/21/lumbung-temple.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/28/11/e1/33/lumbung-temple.jpg',
-    ],
     isFavorite: false,
   ),
   Catering(
@@ -66,12 +45,6 @@ var catringList = [
     'chicken katsu with salad adalah menu yang dibuat sebagai makanan diet namun tetap lezat karena berbahan dari ayam dan di makan bersana salad dengan sayuran yang fresh.',
     type: 'makanan diet',
     imageAsset: 'images/sewu.jpeg',
-    imageUrls: [
-      'https://media-cdn.tripadvisor.com/media/photo-o/16/29/03/ec/dsc-5480-largejpg.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/13/9a/31/8e/caption.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/14/78/fd/64/dsc-0395-largejpg.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/16/10/f6/55/20181224-104447-largejpg.jpg',
-    ],
     isFavorite: false,
   ),
   Catering(
@@ -81,12 +54,6 @@ var catringList = [
     'mushroom with red rice, menu diet yang satu ini merupakan makanan diet berbahan dasar nasi merah dan juga jamur yang diolah menjadi makanan sehat dan juga lezat selain itu menu ini juga sangat digemari banyak orang yang sedang menjalani program diet karna menu sangat sehat dan mengenyangkan.',
     type: 'makanan diet',
     imageAsset: 'images/kalasan.jpeg',
-    imageUrls: [
-      'https://media-cdn.tripadvisor.com/media/photo-o/11/49/7f/c6/img-20171102-100329-largejpg.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/0d/d1/15/1b/papan-nama-peringatan.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/0d/b4/4a/9d/caption.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/0d/bd/3b/88/20161127-152738-largejpg.jpg',
-    ],
     isFavorite: false,
   ),
   Catering(
@@ -96,12 +63,6 @@ var catringList = [
     'beef with kimchi, atau Candi Bendah, beef with kimchi merupakan menu sehat yang berbaha dasar dari daging sapi yanbg diolah menjadi masakan sehat lalu dimakan dengan kimchi yang merupakan olahan dari sayur sawi, menu ini terlihat seperti bukan makanan diet karena tampilannya yang sangat menarik dan juga pastinya enak namun  jumlah kalorinya yang sudah diatur untuk memenuhi kebutuhan program diet.',
     type: 'makanan diet',
     imageAsset: 'images/sari.jpeg',
-    imageUrls: [
-      'http://media-cdn.tripadvisor.com/media/photo-o/0d/7a/26/27/p-20150104-102350-hdr.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/17/e4/b5/32/komplek-candi-sari.jpg',
-      'http://media-cdn.tripadvisor.com/media/photo-o/13/46/0c/0e/cube.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/13/46/09/6d/high-wall.jpg',
-    ],
     isFavorite: false,
   ),
   Catering(
@@ -111,12 +72,6 @@ var catringList = [
     'yelow rice with fried chicken adalah menu yang berbahan dari nasi yang diolah menjadi nasi kuning dan dimakan bersama ayam goreng menu ini sangat nusantara sekali dan sangat cocok dimakan sehari hari walau sedang menajalani program diet menu ini dibuat bertujuan agar orang yang sedang menjalani program diet dapat tetap memakan makanan enak dengan menu nusantara.',
     type: 'makanan diet',
     imageAsset: 'images/plaosan.jpeg',
-    imageUrls: [
-      'https://media-cdn.tripadvisor.com/media/photo-o/15/64/fe/2c/candi-plaosan-nan-exotic.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/0b/9b/16/eb/candi-plaosan-merupakan.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/04/d1/be/8d/candi-plaosan.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/28/14/e5/d4/caption.jpg',
-    ],
     isFavorite: false,
   ),
   Catering(
@@ -126,12 +81,6 @@ var catringList = [
     'salmon with mushed potatto adalah menju yang berbahan dasar ikan salmon fresh yang dimakan dengan mushed potato.',
     type: 'makanan diet',
     imageAsset: 'images/singosari.jpeg',
-    imageUrls: [
-      'https://media-cdn.tripadvisor.com/media/photo-o/06/c9/d4/f2/candi-singosari.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/1a/ca/37/49/candi-cungkup-candirenggo.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/15/a2/c0/8d/dvarapala.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/15/a2/c0/88/singosari-temple-kala.jpg',
-    ],
     isFavorite: false,
   ),
   Catering(
@@ -141,12 +90,6 @@ var catringList = [
     'Rice chicken satay merupakan menu diet berbahan dasar nasi yang diolah dengan sate dada ayam dengan sayuran.',
     type: 'Candi Buddha',
     imageAsset: 'images/jago.jpeg',
-    imageUrls: [
-      'https://media-cdn.tripadvisor.com/media/photo-o/08/75/ab/a3/photo2jpg.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/08/75/ab/a1/photo0jpg.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/05/60/f7/12/jhago-temple.jpg',
-      'https://media-cdn.tripadvisor.com/media/photo-o/05/60/f6/e5/jhago-temple.jpg',
-    ],
     isFavorite: false,
   ),
 ];
